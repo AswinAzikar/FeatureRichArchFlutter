f0afb64bef8d0e3e96a7138badbb4310404c41ca
Aswinazikar <azwinaswin8@gmail.com>
2025-04-09 09:30:35 +0530

    Add Windows runner files for Flutter application
    - Created runner executable manifest to support DPI awareness and compatibility with Windows 10 and 11.
    - Implemented utility functions for console creation, command line argument retrieval, and UTF-16 to UTF-8 string conversion in utils.cpp and utils.h.
    - Developed Win32Window class for managing a high DPI-aware window, including message handling, theme updates, and child content management in win32_window.cpp and win32_window.h.

---
+++ lib/widgets/common_bottom_sheet.dart
@@ -0,0 +1,131 @@
+import 'package:admin_ror/constants/constants.dart';
+import 'package:admin_ror/services/size_utils.dart';
+import 'package:flutter/material.dart';
+
+class CommonBottomSheet extends StatelessWidget {
+  const CommonBottomSheet({
+    super.key,
+    required this.child,
+    this.title,
+    this.popButton,
+    this.maxWidth,
+  });
+
+  final Widget child;
+
+  final String? title;
+
+  final Widget? popButton;
+  final double? maxWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    double bottomPadding = MediaQuery.of(context).viewInsets.bottom;
+    return Material(
+      borderRadius: BorderRadius.vertical(
+        top: Radius.circular(CustomPadding.paddingXL),
+        bottom:
+            SizeUtils.deviceType == DeviceType.mobile
+                ? Radius.zero
+                : Radius.circular(CustomPadding.paddingXL),
+      ),
+      child: Container(
+        constraints: BoxConstraints(
+          maxWidth: maxWidth ?? (context.size!.width< 600 ? double.infinity : 400),
+        ),
+        padding: EdgeInsets.only(bottom: bottomPadding),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  padding:  EdgeInsets.symmetric(vertical: CustomPadding.paddingLarge),
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.vertical(
+                      top: Radius.circular(CustomPadding.paddingXL),
+                    ),
+                    gradient: CustomColors.buttonGradient,
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: CustomPadding.paddingXXL),
+                    child: AutoSizeText(
+                      maxLines: 1,
+                      textAlign: TextAlign.center,
+                      (title ?? ""),
+                      style: context.labelLarge.copyWith(
+                        fontSize:
+                            SizeUtils.deviceType == DeviceType.mobile
+                                ? 20.fSize
+                                : 40.fSize,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  right: custom,
+                  top: 0,
+                  bottom: 0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Spacer(),
+                      popButton ??
+                          InkWell(
+                            borderRadius: BorderRadius.circular(CustomPadding.paddingXL),
+                            onTap: () => Navigator.pop(context),
+                            child: Container(
+                              padding: const EdgeInsets.all(paddingSmall),
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle,
+                              ),
+                              child: const Icon(
+                                Icons.close,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Padding(padding: const EdgeInsets.all(CustomPadding.paddingXL), child: child),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ConfirmationSheet extends StatelessWidget {
+  const ConfirmationSheet({
+    super.key,
+    required this.buttonText,
+    required this.text,
+  });
+
+  final String text;
+  final String buttonText;
+
+  @override
+  Widget build(BuildContext context) {
+    return CommonBottomSheet(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(text),
+          gapLarge,
+          LoadingButton(
+            buttonLoading: false,
+            text: buttonText,
+            onPressed: () => Navigator.pop(context, true),
+          ),
+        ],
+      ),
+    );
+  }
+}
