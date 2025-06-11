ab56d9e381c61dd92f1a070bebd4c9b037d5f686
AswinAzikar <azwinaswin8@gmail.com>
2025-06-11 16:15:26 +0530

    feat: implement exit confirmation dialog and integrate with NavigationScreen

--- lib/features/navigation_screen/navigation_screen.dart
+++ lib/features/navigation_screen/navigation_screen.dart
@@ -1,4 +1,5 @@
 import 'package:FeatureRichArchFlutter/constants/constants.dart';
+import 'package:FeatureRichArchFlutter/services/show_exit_confirmation_dialogue.dart';
 import 'package:flutter/material.dart';
 
 class NavigationScreen extends StatefulWidget {
@@ -11,32 +12,42 @@ class NavigationScreen extends StatefulWidget {
 
 class _NavigationScreenState extends State<NavigationScreen> {
   int _selectedIndex = 0;
+  bool _canPop = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("Page ${_selectedIndex + 1}")),
-      bottomNavigationBar: BottomNavigationBar(
-        enableFeedback: false,
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _selectedIndex,
-        onTap: (index) => setState(() => _selectedIndex = index),
-
-        // Colors
-        selectedItemColor: CustomColors.primaryColor,
-        // unselectedItemColor: Colors.black87,
-        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
-        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
-
-        // Items
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.notifications), label: 'Alerts'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
+    return PopScope(
+      canPop: _canPop,
+      onPopInvokedWithResult: (didPop, result) async {
+        if (!didPop) {
+          final navigator = Navigator.of(context);
+          final confirm = await showExitConfirmationDialog(context);
+          if (!mounted) return;
+          if (confirm) {
+            setState(() => _canPop = true);
+            navigator.maybePop(result);
+          }
+        }
+      },
+      child: Scaffold(
+        body: Center(child: Text("Page ${_selectedIndex + 1}")),
+        bottomNavigationBar: BottomNavigationBar(
+          enableFeedback: false,
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _selectedIndex,
+          onTap: (index) => setState(() => _selectedIndex = index),
+          selectedItemColor: CustomColors.primaryColor,
+          selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+          unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.normal),
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+            BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications), label: 'Alerts'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+        ),
       ),
     );
   }
