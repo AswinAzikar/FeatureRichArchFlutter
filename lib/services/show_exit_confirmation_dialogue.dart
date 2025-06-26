965eda22e65c2c4e0cb66774441403062349e17d
shahilclm <mohammedshahilpkshahil@gmail.com>
2025-06-26 11:18:41 +0530

    feat: replace exit confirmation snackbar with toast notification for improved user experience

--- lib/services/show_exit_confirmation_dialogue.dart
+++ lib/services/show_exit_confirmation_dialogue.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import '../extensions/app_theme_extensions.dart';
 
 DateTime? _lastExitAttempt;
 
@@ -6,22 +9,18 @@ Future<bool> showExitConfirmationToast(BuildContext context) async {
   final now = DateTime.now();
   const exitWarningDuration = Duration(seconds: 2);
 
-  if (_lastExitAttempt == null || now.difference(_lastExitAttempt!) > exitWarningDuration) {
+  if (_lastExitAttempt == null ||
+      now.difference(_lastExitAttempt!) > exitWarningDuration) {
     _lastExitAttempt = now;
+    final appColor = Theme.of(context).extension<AppThemeColors>();
 
-    // Show a chip toast
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: const [
-            Icon(Icons.info, color: Colors.white),
-            SizedBox(width: 8),
-            Text("Tap once more to exit"),
-          ],
-        ),
-        duration: exitWarningDuration,
-      ),
+    Fluttertoast.showToast(
+      msg: "Tap once more to exit",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: appColor!.dynamicIconColor, // You can customize this
+      textColor: appColor.background,
+      fontSize: 14.0,
     );
 
     return Future.value(false); // Do not exit the app
