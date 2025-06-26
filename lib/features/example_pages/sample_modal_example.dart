86573f2899176ba42776974e1d42b3618cb0fbae
AswinAzikar <azwinaswin8@gmail.com>
2025-06-26 15:16:40 +0530

    feat: implement SampleModalExample and update AddScreen layout with navigation buttons

---
+++ lib/features/example_pages/sample_modal_example.dart
@@ -0,0 +1,50 @@
+import 'package:FeatureRichArchFlutter/services/size_utils.dart';
+import 'package:FeatureRichArchFlutter/widgets/mini_loading_button.dart';
+import 'package:flutter/material.dart';
+
+class SampleModalExample extends StatefulWidget {
+  const SampleModalExample({super.key});
+
+  @override
+  State<SampleModalExample> createState() => _SampleModalExampleState();
+}
+
+class _SampleModalExampleState extends State<SampleModalExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sample Modal Example'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          MiniLoadingButton(
+              text: "Tap to show modal ",
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  isScrollControlled: true,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20.0),
+                    ),
+                  ),
+                  builder: (context) {
+                    return Padding(
+                      padding: MediaQuery.of(context).viewInsets,
+                      child: SizedBox(
+                        height: SizeUtils.height * .4,
+                        child: Center(
+                          child: Text('This is a sample modal bottom sheet'),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              })
+        ],
+      ),
+    );
+  }
+}
