1cfd4910b6bf233e04720a9148c687e8897d6bb5
AswinAzikar <azwinaswin8@gmail.com>
2025-12-25 09:29:34 +0000

    fix: update Kotlin plugin version and adjust Gradle init script paths for consistency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,6 @@ android {
     // ndkVersion = '27.0.12077973'
     ndkVersion = "27.0.12077973"
 
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -22,10 +21,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.FeatureRichArchFlutter"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -34,8 +30,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -44,7 +38,8 @@ android {
 flutter {
     source = "../.."
 }
+
 dependencies {
     implementation("com.google.android.gms:play-services-auth:20.7.0")
-    implementation("com.google.android.material:material:1.14.0")
+    implementation("com.google.android.material:material:1.13.0")
 }
